0cd66cd6d441adc3207fb4a73ce7280764826a18
gonghojin <rhdghwls5@naver.com>
2017-11-21 22:47:49 +0900

    cover frag 수정

---
+++ app/src/main/java/test/dahun/mobileplay/adapter/CoverCustomPagerAdapter.java
@@ -0,0 +1,40 @@
+package test.dahun.mobileplay.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import test.dahun.mobileplay.tab.SubCoverView;
+import test.dahun.mobileplay.tab.SubMovieView;
+import test.dahun.mobileplay.ui.PagerAdapter;
+
+public class CoverCustomPagerAdapter extends PagerAdapter {
+
+    Context context;
+    public CoverCustomPagerAdapter(Context context) {
+        this.context=context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+      View v = new SubCoverView(context, position);
+      container.addView(v);
+
+      return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, final int position, Object object) {
+      container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+      return 2;
+    }
+
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+      return arg0 == arg1;
+    }
+}
---
+++ app/src/main/java/test/dahun/mobileplay/tab/SubCoverView.java
@@ -0,0 +1,60 @@
+package test.dahun.mobileplay.tab;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import test.dahun.mobileplay.R;
+
+public class SubCoverView extends LinearLayout {
+
+  //adddd
+  
+  Context context;
+  View view;
+
+  public SubCoverView(Context context, int index) {
+    super(context);
+
+    this.context=context;
+
+    LayoutInflater li = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    view = li.inflate(R.layout.layout_subcover, this, true);
+
+    initSetting(index);
+
+  }
+
+  public void initSetting(final int index) {
+
+    ImageView imv = (ImageView)view.findViewById(R.id.coverimage);
+
+    switch(index){
+      case 0:
+        imv.setImageResource(R.drawable.main_1_bg);
+        break;
+      case 1:
+
+        break;
+
+    }
+  }
+
+}
